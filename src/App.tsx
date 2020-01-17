c093772bd68eda733aa57d8cd9821480b98fdfb6
fanfan <fatfan1020@gmail.com>
2020-01-17 10:19:23 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,18 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const App: React.FC = () => {
+const App = (props: object) => {
+  const [count, setCount] = useState<number>(1)
+  const handleCount = () => {
+    setCount(count + 1)
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={handleCount}>
+          {count}
+        </button>
       </header>
     </div>
   );
